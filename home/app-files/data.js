e746ee591806389328a4de2ce36eb9296f0a78ff
MOI-77 <kiyi.72.86@gmail.com>
2025-11-13 11:40:57 +0800

    123

---
+++ home/app-files/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--360",
+      "name": "澀谷中心 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.8832667002301946,
+          "pitch": -0.16824951122874587,
+          "title": "澀谷事變開始後主要戰鬥地",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
